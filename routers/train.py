01230a436b55901fc7005eeee938a64a1c02a497
YoonGangHoon <yghun021007@naver.com>
2025-05-22 02:34:55 +0900

    Feat: csv 파일을 여러 개 받을 수 있도록 수정

--- routers/train.py
+++ routers/train.py
@@ -8,13 +8,14 @@ from train.xgb_utils.train_xgboost import train_xgboost
 from train.run_kmeans_clustering import run_kmeans_clustering
 import requests
 from config import config
+from typing import List
 
 train_router = APIRouter(prefix="/train", tags=["Training"])
 
 @train_router.post("/cluster")
-async def train_clustering(train_file: UploadFile = File(...)):
+async def train_clustering(train_file: List[UploadFile] = File(...)):
     try:
-        df = read_csv_upload_file(train_file)
+        df = read_csv_upload_file(train_file[0])
         cluster_result = run_kmeans_clustering(df)
         print(cluster_result)
         for store_id in cluster_result:
@@ -29,9 +30,9 @@ async def train_clustering(train_file: UploadFile = File(...)):
         return JSONResponse(content={"error": str(ve)}, status_code=400)
 
 @train_router.post("/prophet")
-async def train_prophet(train_file: UploadFile = File(...)):
+async def train_prophet(train_file: List[UploadFile] = File(...)):
     try:
-        df = read_csv_upload_file(train_file)
+        df = read_csv_upload_file(train_file[0])
         for store_id, store_df in df.groupby("store_id"):
             cluster_id = store_df["cluster_id"].iloc[0]
             prophet_func = get_prophet_function(cluster_id)
@@ -42,13 +43,24 @@ async def train_prophet(train_file: UploadFile = File(...)):
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
 @train_router.post("/xgboost")
-async def train_xgboost_endpoint(train_file: UploadFile = File(...)):
+async def train_xgboost_endpoint(train_file: List[UploadFile] = File(...)):
     try:
-        df = read_csv_upload_file(train_file)
-        df["date"] = pd.to_datetime(df["date"])
-        df_with_target = compute_yhat_and_target(df)
-        df_features = generate_features(df_with_target)
-        train_xgboost(df_features)
+        if len(train_file) != 2:
+            return JSONResponse(content={"error": f"2개의 파일이 필요합니다. 현재 {len(train_file)}개 수신됨"}, status_code=400)
+        
+        # 2년 치 판매 데이터
+        sales_df = read_csv_upload_file(train_file[0])
+        sales_df["date"] = pd.to_datetime(sales_df["date"])
+
+        # 1년 치 날씨 데이터
+        weather_df = read_csv_upload_file(train_file[1])
+        weather_df["date"] = pd.to_datetime(weather_df["date"])
+
+        # 여기는 알아서 수정
+        # df_with_target = compute_yhat_and_target(df)
+        # df_features = generate_features(df_with_target)
+        # train_xgboost(df_features)
+
         return JSONResponse(content={"message": "XGBoost 학습 및 저장 완료"}, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
