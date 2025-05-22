17cd8a92469ef5cdf5d6921751db7896b55d507d
pupuReady <cwoo505@naver.com>
2025-05-23 00:02:29 +0900

    Feat: 이상치 제거 로직을 Prophet의 신뢰구간을 이용하도록 개선

--- routers/train.py
+++ routers/train.py
@@ -9,6 +9,7 @@ from train.run_kmeans_clustering import run_kmeans_clustering
 import requests
 from config import config
 from typing import List
+from datetime import timedelta
 
 train_router = APIRouter(prefix="/train", tags=["Training"])
 
@@ -56,11 +57,26 @@ async def train_xgboost_endpoint(train_file: List[UploadFile] = File(...)):
         weather_df = read_csv_upload_file(train_file[1])
         weather_df["date"] = pd.to_datetime(weather_df["date"])
 
-        # 여기는 알아서 수정
-        # df_with_target = compute_yhat_and_target(df)
-        # df_features = generate_features(df_with_target)
-        # train_xgboost(df_features)
+        # 매출 데이터가 적은 Store drop
+        sales_df["date"] = pd.to_datetime(sales_df["date"])
+        store_lengths = sales_df.groupby("store_id")["date"].agg(["min", "max"])
+        store_lengths["delta"] = store_lengths["max"] - store_lengths["min"]
+        store_lengths["delta_days"] = store_lengths["delta"].dt.days
+        insufficient_ids = store_lengths[store_lengths["delta_days"] < 700].index.tolist()
+        sales_df = sales_df[~sales_df["store_id"].isin(insufficient_ids)]
+        weather_df = weather_df[~weather_df["store_id"].isin(insufficient_ids)]
 
+        # yhat, y, 신뢰구간 생성 
+        df_yhat = compute_yhat_and_target(sales_df)
+
+        # 이를 날씨 정보와 병합 
+        df_merged = pd.merge(df_yhat, weather_df, on=["store_id", "date"], how="left")
+        df_merged.to_csv("yhat.csv", index=False)
+        # 최종 xgboost Input 생성
+        df_xgboost = generate_features(df_merged)
+
+        # XGBoost 학습
+        train_xgboost(df_xgboost)  
         return JSONResponse(content={"message": "XGBoost 학습 및 저장 완료"}, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
